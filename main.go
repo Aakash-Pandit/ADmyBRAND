66bee2026cac2462014386cf190ec7c7006da4d7
Aakash-Pandit <aakashpandit366@gmail.com>
2022-09-24 06:22:53 +0530

    implements swager

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Aakash-Pandit/ADmyBRAND/routes"
 	"github.com/Aakash-Pandit/ADmyBRAND/storage"
 	"github.com/gin-gonic/gin"
+	"github.com/mvrilo/go-redoc"
+	ginredoc "github.com/mvrilo/go-redoc/gin"
 )
 
 func ApiDoc(context *gin.Context) {
@@ -15,13 +17,13 @@ func ApiDoc(context *gin.Context) {
 
 func main() {
 
-	// doc := redoc.Redoc{
-	// 	Title:       "Example API",
-	// 	Description: "Example API Description",
-	// 	SpecFile:    "./openapi.json",
-	// 	SpecPath:    "/openapi.json",
-	// 	DocsPath:    "/docs",
-	// }
+	doc := redoc.Redoc{
+		Title:       "Example API",
+		Description: "Example API Description",
+		SpecFile:    "./Aakash-Pandit-API-Documentation-v1.0.0-resolved.json",
+		SpecPath:    "/Aakash-Pandit-API-Documentation-v1.0.0-resolved.json",
+		DocsPath:    "/docs",
+	}
 
 	_, err := storage.NewConnection()
 	if err != nil {
@@ -29,6 +31,7 @@ func main() {
 	}
 
 	route := gin.Default()
+	route.Use(ginredoc.New(doc))
 	route.LoadHTMLGlob("templates/*.html")
 	routes.SetupRoutes(route)
 	route.Run()
